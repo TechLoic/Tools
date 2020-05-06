cf9ef758c9b209bd6bdbcab475414101cf50989c
Loweack <loweack@gmail.com>
2020-05-06 09:56:44 +0200

    OK

--- Get-Warranty/Get-Warranty.ps1
+++ Get-Warranty/Get-Warranty.ps1
@@ -10,17 +10,21 @@ if ($brand -eq 'Dell Inc.')
 	$WarrantyEndDate = Get-ItemProperty -Path HKLM:\SOFTWARE\DELL\WARRANTY | Select-Object -ExpandProperty WarrantyEndDate
 	$WarrantySupportLevel = Get-ItemProperty -Path HKLM:\SOFTWARE\DELL\WARRANTY | Select-Object -ExpandProperty WarrantySupportLevel
 	
-	$wshell = New-Object -ComObject Wscript.Shell
-	$wshell.Popup("$env:computername est sous garantie jusqu'au $WarrantyEndDate`n`n[ Information ]`nModel : $Model`nServiceTag : $ServiceTag`nWarranty Start Date : $WarrantyStartDate`nSupport Level : $WarrantySupportLevel",0,"Information",0x40)
-	
-    Exit
+	Add-Type -AssemblyName PresentationCore,PresentationFramework
+    $ButtonType = [System.Windows.MessageBoxButton]::Ok
+    $MessageIcon = [System.Windows.MessageBoxImage]::Information
+    $MessageBody = "$env:computername est sous garantie jusqu'au $WarrantyEndDate`n`n[ Details ]`nModel : $Model`nServiceTag : $ServiceTag`nWarranty Start Date : $WarrantyStartDate`nSupport Level : $WarrantySupportLevel"
+    $MessageTitle = "Information"
+    $Result = [System.Windows.MessageBox]::Show($MessageBody,$MessageTitle,$ButtonType,$MessageIcon)
 }
 
 else 
 {
     # Error if not a Dell computer
-    $wshell = New-Object -ComObject Wscript.Shell
-	$wshell.Popup("Le PC $env:computername n'est pas un ordinateur Dell",0,"Erreur",0x30)
-
-    Exit
+	Add-Type -AssemblyName PresentationCore,PresentationFramework
+    $ButtonType = [System.Windows.MessageBoxButton]::Ok
+    $MessageIcon = [System.Windows.MessageBoxImage]::Error
+    $MessageBody = "$env:computername n'est pas un ordinateur Dell"
+    $MessageTitle = "Erreur"
+    $Result = [System.Windows.MessageBox]::Show($MessageBody,$MessageTitle,$ButtonType,$MessageIcon)
 }
